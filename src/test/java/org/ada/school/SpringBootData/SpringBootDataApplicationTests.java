e4a589b131ec0887deb0bbfb842b285b6a1b079c
DavidRiveraRvD <54150584+DavidRiveraRvD@users.noreply.github.com>
2021-08-28 20:56:59 -0500

    Commit

---
+++ src/test/java/org/ada/school/SpringBootData/SpringBootDataApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ada.school.SpringBootData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
